85865ebebfb2eaf9b148aa5302dfc1c316bf0c7e
Jeffrey Lovitz <jeffrey.lovitz@gmail.com>
2021-01-08 12:50:46 -0500

    Address PR comments

--- tests/tck/utils/assertions.py
+++ tests/tck/utils/assertions.py
@@ -85,8 +85,13 @@ def pathToString(pathToConvert):
     return strValue
 
 def dictToString(dictToConvert):
+    size = len(dictToConvert)
     strValue = '{'
-    strValue += ", ".join(map(lambda kv: kv[0] + ": " + toString(kv[1]), dictToConvert.iteritems()))
+    for idx, item in enumerate(dictToConvert.items()):
+        strValue += item[0] + ": "
+        strValue += toString(item[1])
+        if idx < size - 1:
+            strValue += ", "
     strValue += '}'
     return strValue
 
@@ -111,6 +116,8 @@ def toString(value):
         return listToString(value)
     elif isinstance(value, Path):
         return pathToString(value)
+    elif isinstance(value, dict):
+        return dictToString(value)
     elif value == None:
         return "null"
 
